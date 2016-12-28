f8dc0c574fe9231ddc691823abf22730d0e2e693
Amr Fathy <amruafathy@gmail.com>
2016-12-28 08:49:52 +0200

    Model fixes

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,11 +7,7 @@ class Post < ApplicationRecord
     has_attached_file :photo, styles: {medium: "300x300>", thumb: "100x100>", large: '900x300>'}
     validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
 
-    counter_culture :user
     acts_as_votable
-
-    include PublicActivity::Model
-    tracked only: [:create, :like], owner: proc { |_controller, model| model.user }
 
     default_scope -> { order('created_at DESC') }
 end
